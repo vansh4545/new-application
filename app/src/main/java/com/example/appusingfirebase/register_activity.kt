c31d7e3c0665165dab9362804f3e0b4fd98df97a
vansh4545 <vanshgupta4545@gmail.com>
2023-03-08 22:40:26 +0530

    new commit

---
+++ app/src/main/java/com/example/appusingfirebase/register_activity.kt
@@ -0,0 +1,11 @@
+package com.example.appusingfirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class register_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
